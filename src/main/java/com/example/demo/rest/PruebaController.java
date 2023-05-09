9c9fc7ef9bc9b5b0cddb08dedbf7f1d853280c47
DiegoRestrepo1998 <dfrestrepo1998@gmail.com>
2023-05-09 17:16:13 -0500

    Commit Final CI/CD AWS

--- src/main/java/com/example/demo/rest/PruebaController.java
+++ src/main/java/com/example/demo/rest/PruebaController.java
@@ -10,6 +10,6 @@ public class PruebaController{
 
     @GetMapping
     public String helloWorld(){
-        return "Hello World. probando CI/CD automaticos. Ahora debe publicar de manera automatica";
+        return "Hello World. Realizando prueba final 09052023 5:15 pm";
     }
 }
