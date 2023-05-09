6d740b32b80174bde8690754f34633d0d6d9b145
DiegoRestrepo1998 <94764819+DiegoRestrepo1998@users.noreply.github.com>
2023-05-09 17:35:51 -0500

    Update PruebaController.java

--- src/main/java/com/example/demo/rest/PruebaController.java
+++ src/main/java/com/example/demo/rest/PruebaController.java
@@ -10,6 +10,6 @@ public class PruebaController{
 
     @GetMapping
     public String helloWorld(){
-        return "Hello World. Realizando prueba final 09052023 5:21 pm";
+        return "Hello World. Realizando prueba final 09052023 5:35 pm";
     }
 }
