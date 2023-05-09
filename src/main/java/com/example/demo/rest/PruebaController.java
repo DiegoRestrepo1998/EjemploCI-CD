087e462a0d696b05773cc21f774b3679d9643b09
DiegoRestrepo1998 <dfrestrepo1998@gmail.com>
2023-05-09 18:00:22 -0500

    prueba generacion de Jar

--- src/main/java/com/example/demo/rest/PruebaController.java
+++ src/main/java/com/example/demo/rest/PruebaController.java
@@ -10,6 +10,6 @@ public class PruebaController{
 
     @GetMapping
     public String helloWorld(){
-        return "Hello World. Realizando prueba final 09052023 5:50 pm";
+        return "Hello World. Realizando prueba final 09052023 5:59 pm";
     }
 }
