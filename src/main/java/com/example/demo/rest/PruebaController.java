cc3c3b5eb4624324ea8e73ae0ccaba22d4d028a8
DiegoRestrepo1998 <dfrestrepo1998@gmail.com>
2023-05-11 16:50:55 -0500

    Commit

--- src/main/java/com/example/demo/rest/PruebaController.java
+++ src/main/java/com/example/demo/rest/PruebaController.java
@@ -10,6 +10,6 @@ public class PruebaController{
 
     @GetMapping
     public String helloWorld(){
-        return "Hello World. Realizando prueba final 11052023 3:00 pm";
+        return "Hello World. Realizando prueba final 09052023 7:13 pm";
     }
 }
